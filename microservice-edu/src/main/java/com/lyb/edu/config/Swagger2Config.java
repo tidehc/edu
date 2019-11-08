5402ab0e08ad8716189fa511cc584e0f9ba1b46a
734635746 <734635746@qq.com>
2019-11-08 23:50:33 +0800

    配置swagger

---
+++ microservice-edu/src/main/java/com/lyb/edu/config/Swagger2Config.java
@@ -0,0 +1,71 @@
+package com.lyb.edu.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author liuyoubin
+ * @since 2019/11/8 - 23:19
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    /**
+     * 配置后台管理系统课程中心微服务接口文档
+     */
+    @Bean
+    public Docket adminApiConfig(){
+        return  new Docket(DocumentationType.SWAGGER_2)
+                .groupName("adminApi")
+                .apiInfo(adminApiInfo())
+                .select()
+                .paths(Predicates.and(PathSelectors.regex("/admin/.*")))
+                .build();
+    }
+
+    private ApiInfo adminApiInfo(){
+
+        return new ApiInfoBuilder()
+                .title("后台管理系统-课程中心API文档")
+                .description("本文档描述了后台管理系统课程中心微服务接口定义")
+                .version("1.0")
+                .contact(new Contact("liuyoubin", "http://lyb.com", "734635746@qq.com"))
+                .build();
+
+    }
+
+
+    /**
+     * 配置课程中心微服务接口文档
+     */
+    @Bean
+    public Docket webApiConfig(){
+        return  new Docket(DocumentationType.SWAGGER_2)
+                .groupName("webApi")
+                .apiInfo(webApiInfo())
+                .select()
+                .paths(Predicates.not(PathSelectors.regex("/admin/.*")))
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build();
+    }
+
+    private ApiInfo webApiInfo(){
+
+        return new ApiInfoBuilder()
+                .title("网站-课程中心API文档")
+                .description("本文档描述了课程中心微服务接口定义")
+                .version("1.0")
+                .contact(new Contact("liuyoubin", "http://lyb.com", "734635746@qq.com"))
+                .build();
+
+    }
+}
--- microservice-edu/src/main/java/com/lyb/edu/controller/admin/TeacherAdminController.java
+++ microservice-edu/src/main/java/com/lyb/edu/controller/admin/TeacherAdminController.java
@@ -2,6 +2,9 @@ package com.lyb.edu.controller.admin;
 
 import com.lyb.edu.entity.Teacher;
 import com.lyb.edu.service.TeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin/edu/teacher")
 @CrossOrigin
+@Api(description = "讲师管理")
 public class TeacherAdminController {
 
     @Autowired
@@ -23,16 +27,20 @@ public class TeacherAdminController {
     /**
      * 查询所有教师
      */
+    @ApiOperation(value = "所有讲师列表")
     @GetMapping
     public List<Teacher> list(){
         return teacherService.list(null);
     }
 
     /**
-     * 通过id删除教师
+     * 通过id删除讲师
      */
+    @ApiOperation(value = "通过id删除讲师")
     @DeleteMapping("/{id}")
-    public boolean removeById(@PathVariable(value = "id") String id){
+    public boolean removeById(
+            @ApiParam(value = "讲师id")
+            @PathVariable(value = "id") String id){
         return teacherService.removeById(id);
     }
 }
